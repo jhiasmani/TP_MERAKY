d63d1e7270903667c01554d74b92d9e7c6626918
jhiasmani <145514678+jhiasmani@users.noreply.github.com>
2024-05-19 12:43:12 -0300

    correcion  de meraki.js

--- js/miraki.js
+++ js/miraki.js
@@ -1,29 +1,27 @@
-const formCarga = document.getElementById("form-cargar")
+const formCarga = document.getElementById("form_contacto")
 
 function RecibirConsulta(e){
     //cancelamos el evento por default
     e.preventDefault()
     //leer datos del form
-    let cNombre = document.getElementById("nombre").value
-    let cCelular = document.getElementById("celular").value
-    let cEmail = document.getElementById("email").value
-    let cCliente = document.getElementById("cliente").value
-    let cConsulta = document.getElementById("consulta").value
-    document.write
+    let coNombre = document.getElementById("nombreCt").value
+    let cCelular = document.getElementById("celularCt").value
+    let cEmail = document.getElementById("emailCt").value
+    let cCliente = document.getElementById("clienteCt").value
+    let cConsulta = document.getElementById("consultaCt").value
+    respuesta.innerHTML=""
     respuesta.innerHTML+=`
-        <div >
-            <div >
-                <span>Recibimos tu consulta</span>
+        <div>
+            <div>
+                <span>Recibimos tu consulta. </span>
                 <span>Gracias</span>
-                <h3> ${cNombre} !!!
-                </h3>
+                <h3> ${coNombre} !!!</h3>
             </div>
             
-        </div>
-        `
-    }
-    formCarga.addEventListener("submit",RecibirConsulta)   
+        </div>`
 
-    const formCompras = document.getElementById("procesar_compra")
+}
 
-        
+    formCarga.addEventListener("submit",RecibirConsulta)
+
+    
